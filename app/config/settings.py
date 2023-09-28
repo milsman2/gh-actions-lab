125ac5b1befbc1139d506cd4be2f7a71427c13d2
Miles <milsman2@gmail.com>
2023-09-28 01:04:58 +0000

    fix url type.

--- app/config/settings.py
+++ app/config/settings.py
@@ -3,8 +3,8 @@ Configuration settings for the application.
 """
 
 from pydantic_settings import BaseSettings
-from pydantic import AnyHttpUrl
+from pydantic import HttpUrl
 
 
 class Settings(BaseSettings):
-    TEST_URL: AnyHttpUrl = "https://api.sunrisesunset.io/json?lat=29.7604&lng=-95.3698&timezone=UTC&date=today"
+    TEST_URL: HttpUrl = "https://api.sunrisesunset.io/json?lat=29.7604&lng=-95.3698&timezone=UTC&date=today"
