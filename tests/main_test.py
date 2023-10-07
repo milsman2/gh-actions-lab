3e407c78ed06da4dac897fc5612cbe59ca71fd48
Miles <milsman2@gmail.com>
2023-10-07 19:20:26 +0000

    Fix import again.

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,11 +1,11 @@
 """
 Testing settings.
 """
-from src.config import settings
+from src.config import app_settings
 from pydantic import HttpUrl
 
 
 def test_settings():
-    assert settings.TEST_URL == HttpUrl(
+    assert app_settings.TEST_URL == HttpUrl(
         "https://api.sunrisesunset.io/json?lat=29.7604&lng=-95.3698&timezone=America/Chicago&date=today"
     )
