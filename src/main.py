e167d8b39632f5b86a013d36cc12d881ee957e25
Miles <milsman2@gmail.com>
2023-10-08 18:24:01 +0000

    testing types.

--- src/main.py
+++ src/main.py
@@ -5,29 +5,32 @@ import asyncio
 
 from icecream import ic
 from pydantic import ValidationError
-from result import Err, Ok
+from result import Err, Ok, Result
 
 from src.schemas import SunResults
 from src.client import AioHttpClient
 from src.config import app_settings
 
 
-async def get_sun_times() -> None:
+async def get_sun_times() -> Result[SunResults, str]:
     ic()
     async with AioHttpClient() as http_client:
         ic()
         results = await http_client.get_data(str(app_settings.TEST_URL))
         match results:
             case Ok(data):
+                ic(type(data))
                 try:
-                    SunResults(**data)
+                    sun_data = SunResults(**data)
                 except ValidationError as e:
                     ic(e)
+                    return Err(str(e))
                 else:
-                    ic(data)
-                    ic(SunResults(**data))
+                    ic(sun_data)
+                    return Ok(sun_data)
             case Err(err):
                 ic(err)
+                return Err(err)
 
 
 async def main():
