bacb6ca8e3b58b77a2d632f4ffdec0a5158c2f99
milsman2 <milsman2@gmail.com>
2023-11-22 16:55:33 -0600

    Select specific Python 3.10 in actions.

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,9 +2,8 @@
 Configuration settings for the application.
 """
 
-from pydantic import HttpUrl
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    TEST_URL: HttpUrl = "https://api.sunrisesunset.io/json?lat=29.7604&lng=-95.3698&timezone=America/Chicago&date=today"
+    TEST_URL: str = "https://api.sunrisesunset.io/json?lat=29.7604&lng=-95.3698&timezone=America/Chicago&date=today"
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,13 +1,12 @@
 """
 Testing settings.
 """
-from pydantic import HttpUrl
 from src.client import AioHttpClient
 from src.config import app_settings
 
 
 def test_settings():
-    assert app_settings.TEST_URL == HttpUrl(
+    assert app_settings.TEST_URL == (
         "https://api.sunrisesunset.io/json?lat=29.7604&lng=-95.3698&timezone=America/Chicago&date=today"
     )
 
