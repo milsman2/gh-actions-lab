9274655231dbba4d7a114402a36118f230f62219
Miles <milsman2@gmail.com>
2023-11-25 02:59:27 +0000

    default case

--- src/config/settings.py
+++ src/config/settings.py
@@ -8,4 +8,7 @@ from typing import Optional
 
 
 class Settings(BaseSettings):
-    TEST_URL: Optional[str] = os.getenv("TEST_URL")
+    TEST_URL: Optional[str] = os.getenv(
+        "TEST_URL",
+        "https://api.sunrisesunset.io/json?lat=29.7604&lng=-95.3698&timezone=America/Chicago&date=today",
+    )
